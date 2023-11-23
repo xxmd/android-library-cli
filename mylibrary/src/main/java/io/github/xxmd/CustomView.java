29e54c9f9519d033f1d436425e94d70ffc0990e3
GMJ <410515742@qq.com>
2023-11-23 14:19:04 +0800

    first commit

---
+++ mylibrary/src/main/java/io/github/xxmd/CustomView.java
@@ -0,0 +1,17 @@
+package io.github.xxmd;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class CustomView extends View {
+    public CustomView(Context context) {
+        super(context);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
